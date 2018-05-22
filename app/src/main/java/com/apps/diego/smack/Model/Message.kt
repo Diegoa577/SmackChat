dacc6b4d93afeeb11e3bb60b761fe96796e98454
Diegoa577 <diego.andres420@gmail.com>
2018-05-21 19:48:44 -0500

    Chat

---
+++ app/src/main/java/com/apps/diego/smack/Model/Message.kt
@@ -0,0 +1,5 @@
+package com.apps.diego.smack.Model
+
+class Message constructor(val message: String, val userName: String, val channelId: String,
+                          val userAvatar: String, val userAvatarColor: String,
+                          val id: String, val timeStamp: String)
---
+++ app/src/main/java/com/apps/diego/smack/Utilities/Constants.kt
@@ -0,0 +1,16 @@
+package com.apps.diego.smack.Utilities
+
+
+
+const val BASE_URL = "https://chatty577.herokuapp.com/v1/"
+//const val BASE_URL = "http://10.0.2.2:3005/v1/"
+const val SOCKET_URL = "https://chatty577.herokuapp.com/"
+const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
+const val URL_CREATE_USER = "${BASE_URL}user/add"
+const val URL_GET_USER = "${BASE_URL}user/byEmail/"
+const val URL_GET_CHANNELS = "${BASE_URL}channel/"
+const val URL_GET_MESSAGES = "${BASE_URL}message/byChannel/"
+
+// Broadcast Constants
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
